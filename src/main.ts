f8422574e98c44495b6bf74c8b872d8e3495a22d
Ashikur Rahman Nabir <nabir.rahman@selisegroup.com>
2026-06-02 19:42:14 +0600

    samll fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { createNestApp } from './bootstrap-app';
 async function bootstrap() {
   const app = await createNestApp();
   // await app.listen(3000); // dev
-  await app.listen(4000); // prod
+  await app.listen(3000); // prod
 }
 bootstrap();
